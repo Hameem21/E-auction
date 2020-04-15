7803ccef52677024e1e003864d50ae3b4e9f1aa7
Naheeda Rahman <naheeda9@outlook.com>
2020-04-15 18:33:45 +0100

    Added testing and class for report management

---
+++ E-auction-testing/clsReport.cs
@@ -0,0 +1,9 @@
+﻿namespace E_auction_testing
+{
+    internal class clsReport
+    {
+        public clsReport()
+        {
+        }
+    }
+}
---
+++ E-auction-testing/tstReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace E_auction_testing
+{
+    [TestClass]
+    public class tstReport
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsReport AReport = new clsReport();
+            //test to see that it exists
+            Assert.IsNotNull(AReport);
+        }
+    }
+}
